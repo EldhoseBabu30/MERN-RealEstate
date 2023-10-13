f0cbfd03a393bfe4b0cbcc57fbe9edb0bcf6083d
Eldhose Babu <eldhosebabu316@gmail.com>
2023-10-13 12:32:52 +0530

    complete signup page ui

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,19 +1,20 @@
-import User from "../models/user.model.js"
+import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
-import { errorHandler } from "../utils/error.js"
-errorHandler
+// import { errorHandler } from '../utils/error.js';
+export const signup=async(req,res,next)=>{
 
-export const signup = async (req, res, next) => {
-    const {username, email, password} = req.body
-    const hashedPassword = bcryptjs.hashSync(password, 10)
-    const newUser = new User({username,email, password: hashedPassword})
+const {username,email,password}=req.body;
+const hashedPassword = bcryptjs.hashSync(password,10);
+const newUser= new User({username, email, password: hashedPassword});
+try{
+await newUser.save();
+ res.status(201).json('User created successfully!');
+}
+catch(error){
+    next(error);
+// next(errorHandler(550,'error from the function '));
+}
 
-    try {
-        await newUser.save()
-        res.status(201).json("user created successfully")
-    } catch (error) {
-        next(error)
-    }
-   
-   
+
+
 }
--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,34 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import userRouter from './routes/user.route.js'
+import dotenv from 'dotenv';
+import userRouter from'./routes/user.route.js';
 import authRouter from './routes/auth.route.js'
 
-import dotenv from 'dotenv';
 dotenv.config();
 
-mongoose.connect(process.env.MONGO)
-.then(()=> {
-    console.log('Connected to MongoDb');
-})
-.catch((err) => {
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('connected to mongoDB!');
+}).catch((err)=>{
     console.log(err);
 });
+ 
+const app = express();
 
+app.use(express.json());
 
- const app = express();
- app.use(express.json());
+app.listen(3000,()=>{
+    console.log("server runing on port 3000"); 
+}) 
 
- app.listen(3000, ()=>{
-    console.log("Server is running on port 3000");
- })
+app.use("/api/user", userRouter)
+app.use("/api/auth",authRouter)
 
- app.use('/api/user', userRouter);
- app.use('/api/auth', authRouter);
- app.use((err, req, res, next) =>{
-    const statusCode = err.statusCode || 500 ;
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
     const message = err.message || 'internal server error';
     return res.status(statusCode).json({
         success:false,
         statusCode,
-        message,
-    })
- })
+        message
+    });
+});
