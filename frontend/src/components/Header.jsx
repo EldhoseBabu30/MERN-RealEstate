7ea9caec5c275c41914f92276006a112a94892c0
Eldhose Babu <eldhosebabu316@gmail.com>
2023-10-19 16:54:46 +0530

    update the header and make profile page prIvate

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import {FaSearch} from'react-icons/fa'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux';
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+  
   return (
   
 
@@ -26,9 +29,19 @@ export default function Header() {
           <Link to='/about'>
           <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
          </Link>
-         <Link to='/sign-in'>
-          <li className='sm:inline text-slate-700 hover:underline'>Sign In</li>
+         <Link to='/profile'>
+            {currentUser ? (
+              <img className='rounded-full h7 w-7 object cover'
+               
+                src={currentUser.avatar}
+                alt='profile'
+              />
+            ) : (
+              <li className=' text-slate-700 hover:underline'> Sign in</li>
+            )}
           </Link>
+          
+         
   
         </ul>
       </div>
